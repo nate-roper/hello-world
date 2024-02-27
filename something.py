5b63bb5f2320df0d9cff9443e320e5d6bb679f40
Nate Roper <njr61@pitt.edu>
2024-02-27 16:30:13 -0500

    Add file something.py

---
+++ something.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+current_year = 2024
+birth_year = current_year - age
+
+print("Hello "+ name + "! You were born in "+ str(birth_year) +".")
